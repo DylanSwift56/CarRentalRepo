fda2a952999c3df68f953dd7fa0b291fbcd91f18
adam-omah <34926918+adam-omah@users.noreply.github.com>
2023-11-07 16:22:04 +0000

    updated rentals to get client details

--- DataAcesssLibrary/IClientData.cs
+++ DataAcesssLibrary/IClientData.cs
@@ -8,5 +8,6 @@ namespace DataAccessLibrary
         Task InsertClient(ClientModel client);
         Task<bool> CheckCredentials(string username, string password);
         Task<int> GetClientIdByEmail(string email);
+        Task<ClientModel> GetClientByID(int clientID);
     }
 }
