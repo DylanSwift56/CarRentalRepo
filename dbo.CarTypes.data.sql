ea6d77fe2db0aad61fdfd3aaf5faaa7c9aadef70
adam-omah <34926918+adam-omah@users.noreply.github.com>
2023-11-10 11:27:36 +0000

    Added Status to car data, and added new car types.

--- dbo.CarTypes.data.sql
+++ dbo.CarTypes.data.sql
@@ -1,1 +1,4 @@
 ﻿insert into dbo.CarTypes (CarType,Description) values('Hatchback','standard 5 door hatchback car');
+insert into dbo.CarTypes (CarType,Description) values('SUV','Suburban Utility Vehicle');
+insert into dbo.CarTypes (CarType,Description) values('Saloon','standard 5 door saloon');
+insert into dbo.CarTypes (CarType,Description) values('Coupe','standard 3 door coupe/sports car');
